405aadeca07b3bf2453132e2e58df37ec4a76c62
Arjun8jadhav <Jadhav8arjun@gmail.com>
2024-11-25 16:02:06 +0530

    background

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,10 @@ function App() {
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const loaderRef = useRef(null); 
+  const [errorMsg, setErrorMsg] = useState("");
 
-
-  
   const fetchImages = async () => {
-    if (loading) return; 
+    if (loading) return; // Avoid duplicate API calls
     setLoading(true);
 
     try {
@@ -18,23 +17,28 @@ function App() {
         `https://api.unsplash.com/photos?page=${page}&client_id=FEuMsNs4ph5eNRgYmCQHW88RHlVGcOkDvIf-eBhwDZk`
       );
       const data = await response.json();
-
- 
+      
+      // Append the fetched images to the existing ones
       setImages((prev) => [...prev, ...data]);
-      setPage((prev) => prev + 1);
+      console.log("Fetched page:", page);
     } catch (error) {
+      setErrorMsg("Error fetching images");
       console.error('Error fetching images:', error);
     } finally {
       setLoading(false);
     }
   };
 
+  useEffect(() => {
+  
+    fetchImages();
+  }, [page]); 
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting) {
-          fetchImages();
+          setPage((prevPage) => prevPage + 1); // Increment page on scroll
         }
       },
       { threshold: 1.0 }
@@ -46,12 +50,12 @@ function App() {
 
     return () => observer.disconnect();
   }, []);
- 
 
   return (
     <div>
       <div className="images-container">
         {images.map((image, index) => (
+          
           <img
             key={`${page}+${image.id}`}
             src={image.urls.small} 
